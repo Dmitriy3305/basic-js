c61dbbc8a007cee023191ba8e2659251fb8f81b6
dmitriy3305 <dmitrijkrymskij6@gmail.com>
2022-05-02 21:31:41 +0400

    getCommonCharacterCount

--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  let Error = 'Unable to determine the time of year!';
+ 
+  }
   // remove line with error and write your code here
-}
-
+  
 module.exports = {
   getSeason
 };
